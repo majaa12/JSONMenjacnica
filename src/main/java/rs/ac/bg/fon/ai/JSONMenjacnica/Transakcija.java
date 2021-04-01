3b69d0b8761552f7237f50c72219e5928a562d1d
majaa12 <majacol98@gmail.com>
2021-04-01 17:43:26 +0200

    Dodata klasa Transakcija sa potrebnim atributima, get i set metodama i metodom toString

---
+++ src/main/java/rs/ac/bg/fon/ai/JSONMenjacnica/Transakcija.java
@@ -0,0 +1,61 @@
+package rs.ac.bg.fon.ai.JSONMenjacnica;
+
+import java.util.GregorianCalendar;
+
+public class Transakcija {
+
+	private String izvornaValuta;
+	private String krajnjaValuta;
+	private double pocetniIznos;
+	private double konvertovaniIznos;
+	private GregorianCalendar datumTransakcije;
+
+	public String getIzvornaValuta() {
+		return izvornaValuta;
+	}
+
+	public void setIzvornaValuta(String izvornaValuta) {
+		this.izvornaValuta = izvornaValuta;
+	}
+
+	public String getKrajnjaValuta() {
+		return krajnjaValuta;
+	}
+
+	public void setKrajnjaValuta(String krajnjaValuta) {
+		this.krajnjaValuta = krajnjaValuta;
+	}
+
+	public double getPocetniIznos() {
+		return pocetniIznos;
+	}
+
+	public void setPocetniIznos(double pocetniIznos) {
+		this.pocetniIznos = pocetniIznos;
+	}
+
+	public double getKonvertovaniIznos() {
+		return konvertovaniIznos;
+	}
+
+	public void setKonvertovaniIznos(double konvertovaniIznos) {
+		this.konvertovaniIznos = konvertovaniIznos;
+	}
+
+	public GregorianCalendar getDatumTransakcije() {
+		return datumTransakcije;
+	}
+
+	public void setDatumTransakcije(GregorianCalendar datumTransakcije) {
+		this.datumTransakcije = datumTransakcije;
+	}
+
+	@Override
+	public String toString() {
+		return "Transakcija [izvornaValuta=" + izvornaValuta + ", krajnjaValuta=" + krajnjaValuta + ", pocetniIznos="
+				+ pocetniIznos + ", konvertovaniIznos=" + konvertovaniIznos + ", datumTransakcije=" + datumTransakcije
+				+ "]";
+	}
+
+	
+}
